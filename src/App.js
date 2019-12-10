9ae2797464c022e00c3748e5c16dd00ea18ed445
Arpan Sharma <arpansharma@zapbuild.com>
2019-12-10 16:20:42 +0530

    single game working

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class Game extends React.Component {
                 className="game"
             >
                 <h1>Welcome</h1>
-                <input
+                {/* <input
                     type="email"
                     onChange={(evt) => {
                         this.setState({
@@ -28,8 +28,14 @@ class Game extends React.Component {
                     value={this.state.email}
                     placeholder={"Please enter your email"}
                     style={{ width: 200, height: 20 }}
-                ></input>
-                <button style={{ margin: 10 }}>Submit</button>
+                ></input> */}
+                <button
+                    onClick={() => {
+                        this.setState({
+                            array: new Array(9).fill(" "),
+                        })
+                    }}
+                    style={{ margin: 10 }}>Reset</button>
 
                 <div>
                     {this.makeSquares()}
@@ -39,19 +45,29 @@ class Game extends React.Component {
     }
 
     makeSquares = () => {
-
         let finalArray = []
         let rowArray = []
         for (let i = 0; i < 9; i++) {
             rowArray.push(
                 <button
+                    onClick={() => {
+                        const array = this.state.array
+                        if (array[i] !== " ")
+                            return
+                        array[i] = this.state.currentX ? "X" : "O"
+                        this.setState({
+                            array: array,
+                            currentX: !this.state.currentX
+                        })
+                        this.checkWinner()
+                    }}
                     style={{
                         height: 50,
                         width: 50,
                         margin: 10
                     }}
                     className="square">
-                    {i}
+                    {this.state.array[i]}
                 </button>)
             if (rowArray.length === 3) {
                 finalArray.push(<div style={{
@@ -72,6 +88,31 @@ class Game extends React.Component {
             }}>
                 {finalArray}
             </div>)
+    }
+
+    checkWinner = () => {
+        this.calculateWinner()
+    }
+
+    calculateWinner() {
+        const squares = this.state.array
+
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] != " " && squares[a] === squares[b] && squares[a] === squares[c]) {
+                alert(this.state.currentX ? "X has won" : "O has won")
+            }
+        }
     }
 
 }
